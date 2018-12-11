2e9883eb64ac878a8c0f81570e511427f16e6b4c
emeryparks <emeryparks09@hotmail.com>
2018-12-11 14:08:23 -0800

    erased a express stuff

---
+++ server.js
@@ -0,0 +1,14 @@
+const PORT = process.env.PORT || 3000;
+
+const io = require('socket.io')(PORT);
+
+io.on('connection', socket => {
+
+  console.log('Connected', PORT, socket.id);
+
+  socket.on('troll', (payload) => {
+    console.log('broadcast', payload);
+    socket.broadcast.emit('incoming', payload);
+  });
+
+});
